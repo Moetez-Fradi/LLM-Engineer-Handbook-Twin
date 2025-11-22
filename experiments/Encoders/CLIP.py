c1a757d8d05d72b1379bf8a9eed171843b39b43c
moetez <moetezfradi7@gmail.com>
2025-11-22 13:25:37 +0100

    Add expirememnting with CLIP model

---
+++ experiments/Encoders/CLIP.py
@@ -0,0 +1,21 @@
+from io import BytesIO
+import requests
+from PIL import Image
+from sentence_transformers import SentenceTransformer
+
+response = requests.get(
+    r"https://github.com/PacktPublishing/LLM-Engineering/blob/main/images/crazy_cat.jpg?raw=true"
+)
+
+image = Image.open(BytesIO(response.content))
+model = SentenceTransformer("clip-ViT-B-32")
+img_emb = model.encode(image)
+text_emb = model.encode(
+    ["A crazy cat smiling.",
+    "A white and brown cat with a yellow bandana.",
+    "A man eating in the garden."]
+)
+print(text_emb.shape)
+
+similarity_scores = model.similarity(img_emb, text_emb)
+print(similarity_scores)
